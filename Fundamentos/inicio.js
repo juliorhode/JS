e24c4f9b280ae2e9738bd4f783f95109bdda059c
Julio Rhode <juliorhode@gmail.com>
2025-03-11 10:20:12 -0400

    Dia 1: Primer codigo con variables

---
+++ Fundamentos/inicio.js
@@ -0,0 +1,13 @@
+// muestra mensaje en consola
+console.log("hola a todos");
+
+// declaracion de variables
+let nombre = "julio"
+const edad = 45
+
+// muestro las variables
+console.log("Mi nombre es: ", nombre);
+console.log("Tengo: ", edad, "años");
+
+
+
